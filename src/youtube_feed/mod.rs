de54f031bf0ed2ea82fb1e4c4df517e23fcaf6f5
Julian Schmidhuber <github@schmiddi.anonaddy.com>
2021-07-30 14:47:58 +0200

    :page_facing_up: Added copyright notices

--- src/youtube_feed/mod.rs
+++ src/youtube_feed/mod.rs
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2021 Julian Schmidhuber <github@schmiddi.anonaddy.com>
+ *
+ * This file is part of Tubefeeder.
+ *
+ * Tubefeeder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tubefeeder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 mod feed;
 
 pub use feed::{Author, Entry, Feed, Link, Media, Thumbnail};
